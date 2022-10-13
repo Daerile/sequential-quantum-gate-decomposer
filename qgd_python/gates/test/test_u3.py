9ee3a04214cd75f107da1a4da3713be3762d8ebc
didi8686 <tazit86@gmail.com>
2022-10-13 15:08:14 +0200

    modify the testing process with qiskit

--- qgd_python/gates/test/test_u3.py
+++ qgd_python/gates/test/test_u3.py
@@ -1,9 +1,17 @@
-# elso feladat: csak a qiskittel létrehozni az u3 kaput, és hattatni egy állapotvektoron
+
 import numpy as np
-from qiskit import QuantumRegister, ClassicalRegister
+from qiskit import QuantumRegister, ClassicalRegister, BasicAer
 from qiskit import QuantumCircuit, execute, IBMQ, transpile
 from qiskit.providers.aer import QasmSimulator
 from qiskit.visualization import plot_histogram
+from qiskit import Aer
+import numpy.linalg as LA
+from qiskit.extensions import UnitaryGate
+ 
+# cerate unitary q-bit matrix
+from scipy.stats import unitary_group
+import numpy as np
+backend = Aer.get_backend('unitary_simulator')
 
 pi=np.pi
 
@@ -11,10 +19,13 @@ q=QuantumRegister(1,'q')
 c=ClassicalRegister(1, 'c')
 
 # Use Aer's qasm_simulator
-simulator = QasmSimulator()
+#simulator = QasmSimulator()
 
 # Create a Quantum Circuit acting on the q register
 circuit = QuantumCircuit(q, c)
+
+#circuit.h(q[0])
+#circuit.cx(q[0], q[1])
 
 # Add the u3 gate on qubit pi, pi, 
 circuit.u(pi,pi, pi/2, q[0])
@@ -22,23 +33,23 @@ circuit.u(pi,pi, pi/2, q[0])
 # Map the quantum measurement to the classical bits
 circuit.measure(q,c)
 
-# compile the circuit down to low-level QASM instructions
-# supported by the backend (not needed for simple circuits)
-compiled_circuit = transpile(circuit, simulator)
+# test the decomposition of the matrix
+## Qiskit backend for simulator
+#backend = Aer.get_backend('unitary_simulator')
 
-# Execute the circuit on the qasm simulator
-job = simulator.run(compiled_circuit, shots=1000)
+# job execution and getting the result as an object
+job = execute(circuit, backend)
 
-# Draw the circuit
-print(circuit)
-
-# Grab results from the job
+# the result of the Qiskit job
 result = job.result()
 
-# Returns counts
-counts = result.get_counts(compiled_circuit)
-print("\nCounts:",counts)
+# the unitary matrix from the result object
+#decomposed_matrix = result.get_unitary(circuit)
+#decomposed_matrix = np.asarray(decomposed_matrix)
 
+# Draw the circuit
+#print(result.get_unitary(circuit,3))
+print(circuit)
 
 
 class Test_u3:
@@ -67,6 +78,7 @@ class Test_u3:
 
         # creating an instance of the C++ class
         U3 = qgd_U3( qbit_num, target_qbit, Theta, Phi, Lambda )
+	#apply_kernel_to( u3_1qbit, input );
 
 ######
 
