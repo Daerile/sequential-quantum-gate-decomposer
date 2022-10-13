fb6ea6ae80cff5cc0f5bebac04bc8eb9ea0dc3f7
didi8686 <tazit86@gmail.com>
2022-10-13 16:04:05 +0200

    modify the testing process with qiskit

--- qgd_python/gates/test/test_u3.py
+++ qgd_python/gates/test/test_u3.py
@@ -1,15 +1,13 @@
-
 import numpy as np
 from qiskit import QuantumRegister, ClassicalRegister, BasicAer
 from qiskit import QuantumCircuit, execute, IBMQ, transpile
 from qiskit.providers.aer import QasmSimulator
 from qiskit.visualization import plot_histogram
 from qiskit import Aer
-import numpy.linalg as LA
-from qiskit.extensions import UnitaryGate
  
-# cerate unitary q-bit matrix
+#cerate unitary q-bit matrix
 from scipy.stats import unitary_group
+
 import numpy as np
 backend = Aer.get_backend('unitary_simulator')
 
@@ -18,38 +16,33 @@ pi=np.pi
 q=QuantumRegister(1,'q')
 c=ClassicalRegister(1, 'c')
 
-# Use Aer's qasm_simulator
-#simulator = QasmSimulator()
-
-# Create a Quantum Circuit acting on the q register
+#Create a Quantum Circuit acting on the q register
 circuit = QuantumCircuit(q, c)
 
-#circuit.h(q[0])
-#circuit.cx(q[0], q[1])
 
-# Add the u3 gate on qubit pi, pi, 
+
+#Add the u3 gate on qubit pi, pi, 
 circuit.u(pi,pi, pi/2, q[0])
 
 # Map the quantum measurement to the classical bits
-circuit.measure(q,c)
-
-# test the decomposition of the matrix
-## Qiskit backend for simulator
-#backend = Aer.get_backend('unitary_simulator')
-
-# job execution and getting the result as an object
-job = execute(circuit, backend)
-
-# the result of the Qiskit job
-result = job.result()
-
-# the unitary matrix from the result object
-#decomposed_matrix = result.get_unitary(circuit)
-#decomposed_matrix = np.asarray(decomposed_matrix)
+#circuit.measure(q,c)
 
 # Draw the circuit
-#print(result.get_unitary(circuit,3))
-print(circuit)
+#print(circuit)
+
+# job execution and getting the result as an object
+job = execute(circuit, backend, shots=8192)
+
+#the result of the Qiskit job
+result=job.result()
+
+# the unitary matrix from the result object
+decomposed_matrix = result.get_unitary(circuit,3)
+decomposed_matrix = np.asarray(decomposed_matrix)
+
+# Draw the circuit
+print(result.get_unitary(circuit,3))
+
 
 
 class Test_u3:
