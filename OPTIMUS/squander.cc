3e59c178aee67a224c739ba8f91944f4f9e81a44
Gregory Morse <gregory.morse@live.com>
2023-04-26 18:22:24 +0200

    Update squander.cc

--- OPTIMUS/squander.cc
+++ OPTIMUS/squander.cc
@@ -12,6 +12,7 @@
 #include "Decomposition_Base.h"
 #include "matrix_real.h"
 #include <math.h>
+#include <sys/syscall.h>
 
 //../bin/OptimusApp --filename=libsquander.so --opt_method=Pso --pso_particles=100 --pso_generations=10
 
@@ -19,9 +20,8 @@ using namespace std;
 
 extern "C"
 {
-/*thread_local*/ N_Qubit_Decomposition_adaptive* decomp = NULL;
-/*thread_local*/ int num_of_parameters = 0;
-thread_local gsl_vector* grad_gsl = NULL;
+N_Qubit_Decomposition_adaptive* decomp = NULL;
+
 
 void    init(QJsonObject data)
 {
@@ -37,14 +37,12 @@ void    init(QJsonObject data)
         Matrix_real optimized_parameters_mtx_loc;
         Gates_block* gate_structure_loc = decomp->determine_initial_gate_structure(optimized_parameters_mtx_loc);
         decomp->combine( gate_structure_loc );
-        num_of_parameters = optimized_parameters_mtx_loc.rows * optimized_parameters_mtx_loc.cols;
-        grad_gsl = gsl_vector_alloc(num_of_parameters);
     }
 }
 
 int	getdimension()
 {
-	return num_of_parameters;
+	return decomp->get_parameter_num();
 }
 void    getmargins(vector<Interval> &x)
 {
@@ -56,26 +54,29 @@ void    getmargins(vector<Interval> &x)
 //f(x)
 double	funmin(vector<double> &x)
 {
-   //return decomp->optimization_problem(x.data());
-   Matrix_real parameters_mtx(x.data(), 1, x.size());
-   return decomp->optimization_problem(parameters_mtx);
+   return decomp->optimization_problem(x.data());
+   //Matrix_real parameters_mtx(x.data(), 1, x.size());
+   //return decomp->optimization_problem(parameters_mtx);
 }
 
 //f'(x)
 void    granal(vector<double> &x,vector<double> &g)
 {
+    gsl_vector* grad_gsl = gsl_vector_alloc(decomp->get_parameter_num());
     gsl_vector gv = { x.size(), sizeof(double), x.data(), NULL, 0 };
     decomp->optimization_problem_grad(&gv, (void*)decomp, grad_gsl);
-    for (size_t i = 0; i < num_of_parameters; i++) {
+    for (size_t i = 0; i < decomp->get_parameter_num(); i++) {
         g[i] = gsl_vector_get(grad_gsl, i);
     }
+    gsl_vector_free(grad_gsl);
 }
 
 QJsonObject    done(vector<double> &x)
 {
     //printf("done\n");
-    /*gsl_vector_free(grad_gsl);
+    /*
     delete decomp;
-    return QJsonObject();*/
+    decomp = NULL;*/
+    return QJsonObject();
 }
 }
