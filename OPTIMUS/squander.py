06b6f307e0f6c452460110a964a255ee69ef9636
Gregory Morse <gregory.morse@live.com>
2023-04-28 01:47:49 +0200

    Update and rename squander.sh to squander.py

---
+++ OPTIMUS/squander.py
@@ -0,0 +1,19 @@
+import timeit, os
+#system("python ../../sequential-quantum-gate-decomposer/saveunitary.py ../../sequential-quantum-gate-decomposer/examples/vqe/19CNOT.qasm ./unitary.binary");
+#5 qbit: one-two-three-v2_100, 4gt10-v1_81, one_two_three-v1_99, one_two_three-v0_98, 4mod7-v1_96, aj_e11_165, alu-v2_32
+#9 qbit: con1_216
+#10 qbit: rd73_140
+folder = '../../ibm_qx_mapping/examples/'
+qasm = '3_17_13'
+levels = 5
+if os.path.exists(qasm + ".out"): os.remove(qasm + ".out")
+for opt_method in ["Bfgs", "Pso", "Genetic", "Multistart", "iPso", "Price", "gende", "de", "Tmlsl", "gcrs",
+    "IntegerGenetic", "ParallelGenetic", "DoubleGenetic", "NeuralMinimizer", "ParallelDe", "parallelPso"]:
+    ret = [None]
+    def runfunc():
+        ret[0] = os.system("hwloc-bind --membind node:0 --cpubind node:0 -- ../bin/OptimusApp --filename=libsquander.so --opt_method=" +
+            opt_method + " --folder=" + folder + " --qasm=" + qasm + " --levels=" + str(levels) + ">>" + qasm + ".out")
+    t = timeit.timeit(runfunc, number=1)
+    if ret[0] != 0: break
+    os.system("echo Total Time " + opt_method + ": " + str(t) + ">>" + qasm + ".out")
+    os.system("tail -n 5 " + qasm + ".out") 
