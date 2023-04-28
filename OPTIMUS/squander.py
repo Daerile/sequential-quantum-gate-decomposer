f7f9707766793a8178e4b58b68c38ccdf3454718
Gregory Morse <gregory.morse@live.com>
2023-04-28 02:11:20 +0200

    Update squander.py

--- OPTIMUS/squander.py
+++ OPTIMUS/squander.py
@@ -4,16 +4,19 @@ import timeit, os
 #9 qbit: con1_216
 #10 qbit: rd73_140
 folder = '../../ibm_qx_mapping/examples/'
-qasm = '3_17_13'
+qasm = '4gt10-v1_81' #'3_17_13'
 levels = 5
 if os.path.exists(qasm + ".out"): os.remove(qasm + ".out")
-for opt_method in ["Bfgs", "Pso", "Genetic", "Multistart", "iPso", "Price", "gende", "de", "Tmlsl", "gcrs",
-    "IntegerGenetic", "ParallelGenetic", "DoubleGenetic", "NeuralMinimizer", "ParallelDe", "parallelPso"]:
+for opt_method in ["NeuralMinimizer", "Bfgs", "Pso", "Genetic", "Multistart", "iPso", "Price", "gende", "de", "Tmlsl", "gcrs",
+    "IntegerGenetic", "ParallelGenetic", "DoubleGenetic", "ParallelDe", "parallelPso"]:
     ret = [None]
     def runfunc():
         ret[0] = os.system("hwloc-bind --membind node:0 --cpubind node:0 -- ../bin/OptimusApp --filename=libsquander.so --opt_method=" +
             opt_method + " --folder=" + folder + " --qasm=" + qasm + " --levels=" + str(levels) + ">>" + qasm + ".out")
     t = timeit.timeit(runfunc, number=1)
-    if ret[0] != 0: print("Error", ret[0]); break
+    if ret[0] != 0 and ret[0] != 34304:
+        print("Error", ret[0]);
+        if ret[0] != 34304: continue #34304 is bad alloc on NeuralMinimizer for large # of parameters
+        break
     os.system("echo Total Time " + opt_method + ": " + str(t) + ">>" + qasm + ".out")
     os.system("tail -n 5 " + qasm + ".out") 
